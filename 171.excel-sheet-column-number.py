7ac35000459ca00a1f44aad2767f92c2d97060b4
prz3fur <furtakprzemyslaw@gmail.com>
2025-09-08 21:02:13 +0200

    order ascii function

---
+++ 171.excel-sheet-column-number.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=171 lang=python3
+#
+# [171] Excel Sheet Column Number
+#
+
+# @lc code=start
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for char in columnTitle:
+            result = result * 26 + (ord(char) - ord('A') + 1)
+        return result
+        
+# @lc code=end
+
