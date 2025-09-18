b077c10c86baddf41203d6b2ac15d04e19f0ceca
prz3fur <furtakprzemyslaw@gmail.com>
2025-09-18 22:42:47 +0200

    F/2025 09 18 2 (#28)
    * vovels reverse
    
    * reversing int

---
+++ 7.reverse-integer.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=7 lang=python3
+#
+# [7] Reverse Integer
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        rev = int(str(abs(x))[::-1]) * sign
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+        return rev
+
+        
+# @lc code=end
+
